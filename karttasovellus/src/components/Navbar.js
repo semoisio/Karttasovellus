1e9065c8b30a7fadef98a216041508a0709ac62f
Severi Moisio <semoisio@uef.fi>
2021-01-10 18:28:14 +0200

    Korjattu otsikko

--- karttasovellus/src/components/Navbar.js
+++ karttasovellus/src/components/Navbar.js
@@ -7,7 +7,7 @@ function Navigation() {
     return (
         <Container fluid className="p-0">
             <Navbar bg="dark" variant="dark" expand="sm">
-                <Navbar.Brand href="" className="pr-5">Näytetyö Hurjalle</Navbar.Brand>
+                <Navbar.Brand href="" className="pr-5">Näytetyö karttasovellus</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
